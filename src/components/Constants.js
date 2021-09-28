71a205bb8b24a4e2f788f5292494216f7896595b
marcusluis <marcusluis.mail@gmail.com>
2021-09-28 14:42:44 -0300

    componente cadastras criado, mas com problemas

--- src/components/Constants.js
+++ src/components/Constants.js
@@ -1,6 +1,6 @@
 export const url = "https://labeninjas.herokuapp.com";
-export const key = {
-    headers: {
-        Authorization : "10839cd6-ca14-4864-8904-8d6cd25b9c19"
-    }
-}
+export const headers = {
+  headers: {
+    Authorization: "10839cd6-ca14-4864-8904-8d6cd25b9c19",
+  },
+};
